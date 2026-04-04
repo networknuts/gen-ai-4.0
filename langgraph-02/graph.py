f23934c253c4954c6087d6a6ba50c3f90408eebf
aryan <arytmw@gmail.com>
2026-04-04 14:43:57 +0000

    added langgraph retry logic code

---
+++ langgraph-02/graph.py
@@ -0,0 +1,147 @@
+from dotenv import load_dotenv
+from typing import TypedDict
+from langchain_openai import ChatOpenAI 
+from langgraph.graph import StateGraph, END 
+import json
+
+# SETUP THE ENVIRONMENT
+load_dotenv()
+
+llm = ChatOpenAI(
+    model="gpt-5.4"
+)
+
+MAX_RETRIES = 3
+
+def llm_json(prompt):
+    response = llm.invoke(
+        "Return only valid json. No markdown.\n" + prompt
+    ).content.strip()
+    return json.loads(response)
+
+
+# DEFINE OUR STATE
+class CodeState(TypedDict):
+    user_request: str
+    code: str
+    rating: int 
+    feedback: str 
+    retries: int
+    status: str # approved or failed 
+
+# NODE 1: DEVELOPER AGENT NODE
+
+def developer_agent(state: CodeState):
+    prompt = f"""
+You are a Node.JS developer. Write intentionally poor Node.JS code for the following requirement:
+{state['user_request']}
+
+If feedback is provided, improve the previous version of the code.
+Previous Code:
+{state['code']}
+
+Feedback:
+{state['feedback']}
+
+Return ONLY the full Node.JS code.
+"""
+    result = llm.invoke(prompt).content
+    return {
+        "code": result,
+        "feedback": ""
+    }
+
+# NODE 2: QA AGENT NODE
+
+def qa_agent(state: CodeState):
+    prompt = f"""
+You are a strict Node.JS QA Engineer.
+Evaluate the following Node.JS code for the given requirements:
+- correctness
+- structure
+- readability
+- best practices
+- error handling
+- scalability
+
+Return JSON only in following format:
+{{
+    "rating": integer between 1-10,
+    "feedback": "clear explanation of improvements to be made"
+}}
+
+Code:
+{state['code']}
+
+"""
+    result = llm_json(prompt)
+    return {
+        "rating": int(result['rating']),
+        "feedback": result['feedback']
+    }
+
+# NODE: SET APPROVED
+def set_approved(state: CodeState):
+    return {"status": "approved"}
+
+# NODE: SET FAILED
+def set_failed(state: CodeState):
+    return {"status": "failed"}
+
+# NODE: INCREMENTAL RETRIES LOGIC
+def increment_retry(state: CodeState):
+    return {"retries": state['retries']+1}
+
+# ROUTER NODE
+def check_rating(state: CodeState):
+    if state['rating'] >= 7:
+        return "approved"
+    if state['retries'] >= MAX_RETRIES:
+        return "failed"
+    return "retry"
+
+# BUILDING THE GRAPH
+
+graph = StateGraph(CodeState)
+
+graph.add_node("developer", developer_agent)
+graph.add_node("qa", qa_agent)
+graph.add_node("approved_node", set_approved)
+graph.add_node("failed_node", set_failed)
+graph.add_node("retry_increment", increment_retry)
+
+graph.set_entry_point("developer")
+graph.add_edge("developer", "qa")
+
+graph.add_conditional_edges(
+    "qa",
+    check_rating,
+    {
+        "approved": "approved_node",
+        "failed": "failed_node",
+        "retry": "retry_increment"
+    }
+)
+graph.add_edge("retry_increment", "developer")
+graph.add_edge("approved_node", END)
+graph.add_edge("failed_node", END)
+
+app = graph.compile()
+
+user_input = input("HUMAN NODEJS APP INPUT: ")
+
+result = app.invoke({
+    "user_request": user_input,
+    "code": "",
+    "rating": 0,
+    "feedback": "",
+    "retries": 0,
+    "status": "running"
+})
+
+print("FINAL RESULT")
+print(f"Status: {result['status']}")
+print(f"Final Rating: {result['rating']}")
+print(f"Retries Used: {result['retries']}")
+print(f"Feedback: {result['feedback']}")
+print(f"Code:\n {result['code']}")
